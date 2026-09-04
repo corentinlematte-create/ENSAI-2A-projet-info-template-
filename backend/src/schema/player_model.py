ba5314b11e267af25fbd57d445a82fa96d9007dd
corentinlematte-create <corentin.lematte@gmail.com>
2026-09-04 00:11:57 +0000

    mon message

--- backend/src/schema/player_model.py
+++ backend/src/schema/player_model.py
@@ -1,3 +1,5 @@
+import os
+
 from pydantic import BaseModel, EmailStr, field_validator
 
 
@@ -17,9 +19,11 @@ class PlayerModel(BaseModel):
     @field_validator("password")
     @classmethod
     def check_password_length(cls, v: str) -> str:
-        min_len = 35
+        min_len = os.environ["PASSWORD_MIN_LENGTH"]
         if len(v) < min_len:
-            raise ValueError(f"Password must be at least {min_len} characters long")
+            raise ValueError(
+                "Password must be at least " + os.environ["PASSWORD_MIN_LENGTH"] + "characters long"
+            )
         return v
 
 
